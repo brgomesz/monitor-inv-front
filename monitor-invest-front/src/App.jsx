cdf926e706d1ce645af9c24900d5a8b8aebc0f04
Bruno Gomes <bruno.rodrigues.gomes@hotmail.com>
2025-05-17 20:16:23 -0300

    Adicionando interface e enviando as variaveis por console.log

---
+++ monitor-invest-front/src/App.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+// import { Box } from "@mui/material";
+import { TextField, Box, Typography, Button } from "@mui/material";
+
+import "./App.css";
+
+function App() {
+  const consoleLog = () => {
+    console.log("Funcionou");
+  };
+  const [dadosCDI, setDadosCDI] = useState([]);
+  const [saldoInicial, setSaldoInicial] = useState();
+  const [aporteMensal, setAporteMensal] = useState();
+  const [mesesAplicacao, setMesesAplicacao] = useState();
+
+  const handleEnviarInfos = () => {
+    console.log({
+      saldoInicial,
+      aporteMensal,
+      mesesAplicacao,
+    });
+  };
+
+  return (
+    <>
+      <Box sx={{ backgroundColor: "#87CEEB", pt: 5 }}>
+        <Box
+          sx={{
+            margin: "auto",
+            p: 3,
+            border: "1px solid #ccc",
+            borderRadius: 2,
+            height: "90vh",
+            width: { xs: "80%", sm: "100%", md: "60%" },
+            backgroundColor: "#ffffff",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              backgroundColor: "#ffffff",
+            }}
+          >
+            {/* <h1 style={{textAlign:'center'}}>Calculadora CDI</h1> */}
+            <Typography
+              sx={{
+                fontSize: { xs: "30px", md: "40px" },
+                textShadow: "2px 2px 2px rgba(0, 0, 0, 0.3)",
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+            >
+              Calculadora CDB
+            </Typography>
+
+            <TextField
+              label="Saldo Inicial"
+              type="number"
+              variant="outlined"
+              onChange={(e) => setSaldoInicial(+e.target.value)}
+            />
+            <TextField
+              label="Aporte Mensal"
+              type="number"
+              variant="outlined"
+              onChange={(e) => setAporteMensal(+e.target.value)}
+            />
+            <TextField
+              label="Meses de Aplicação"
+              type="number"
+              variant="outlined"
+              onChange={(e) => setMesesAplicacao(+e.target.value)}
+            />
+            <Button
+              sx={{
+                backgroundColor: "#1976d2",
+                color: "white",
+                fontWeight: "bold",
+                padding: "10px 20px",
+                borderRadius: "8px",
+                "&:hover": {
+                  backgroundColor: "#1565c0",
+                },
+                width: { xs: "50%", md: "30%" },
+                margin: "auto",
+                mt: 2,
+              }}
+              onClick={handleEnviarInfos}
+            >
+              Enviar
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default App;
